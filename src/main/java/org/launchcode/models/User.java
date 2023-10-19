2c762aa017556dcc073671ab17825e1a7cdb4579
Terri Penn <coderterri@protonmail.com>
2023-10-19 07:22:24 -0500

    Class 12 studio complete including bonus mission

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,23 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Username is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Invalid e-mail.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
+
+    @NotBlank(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
@@ -16,6 +30,11 @@ public class User {
         this.password = password;
     }
 
+    private void checkPassword() {
+        if (this.password != null && this.verifyPassword != null && !this.password.equals(this.verifyPassword)) {
+            this.verifyPassword = null;
+        }
+    }
     public String getUsername() {
         return username;
     }
@@ -38,6 +57,16 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
 
